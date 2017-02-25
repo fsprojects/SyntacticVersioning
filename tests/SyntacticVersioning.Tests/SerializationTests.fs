02d04785dbc474b65c4745addff828e34c3ad768
Oskar Gewalli <oskar@gewalli.se>
2017-02-25 21:37:39 +0100

    Added some tests of serialization and deserialization

--- tests/SyntacticVersioning.Tests/SerializationTests.fs
+++ tests/SyntacticVersioning.Tests/SerializationTests.fs
@@ -4,10 +4,34 @@ open SyntacticVersioning
 open NUnit.Framework
 open TestHelper
 open Chiron
+let assertCanSerializeAndDeserialize assembly
+  =
+    let surface = SurfaceArea.exportedTypes assembly
+    let deserialized 
+        = surface
+          |> Json.serialize
+          |> Json.deserialize
+    let same = deserialized = surface
+    Assert.IsTrue(same)
 [<Test>]
-let ``Should be able to serialize `` () =
-  let surface = SurfaceArea.exportedTypes csharp
-  let json = surface
-              |> Json.serialize
-              |> Json.formatWith JsonFormattingOptions.Pretty
-  Assert.AreEqual( "<>", json)
+let ``csharp`` () =
+  assertCanSerializeAndDeserialize csharp
+[<Test>]
+let ``csharp2`` () =
+  assertCanSerializeAndDeserialize csharp2
+[<Test>]
+let ``enum`` () =
+  assertCanSerializeAndDeserialize enum
+[<Test>]
+let ``enum2`` () =
+  assertCanSerializeAndDeserialize enum2
+[<Test>]
+let ``enum3`` () =
+  assertCanSerializeAndDeserialize enum3
+[<Test>]
+let ``fsharp`` () =
+  assertCanSerializeAndDeserialize fsharp
+[<Test>]
+let ``fsharp2`` () =
+  assertCanSerializeAndDeserialize fsharp2
+
