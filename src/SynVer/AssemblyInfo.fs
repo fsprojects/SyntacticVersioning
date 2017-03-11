f8bb4e8abf875ff1dd2e37c39bdb267aae5e8743
Oskar Gewalli <oskar@gewalli.se>
2017-03-11 14:17:32 +0100

    Pushed to NuGet

--- src/SynVer/AssemblyInfo.fs
+++ src/SynVer/AssemblyInfo.fs
@@ -3,7 +3,7 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("SynVer")>]
-[<assembly: AssemblyProductAttribute("SynVer")>]
+[<assembly: AssemblyProductAttribute("SyntacticVersioning")>]
 [<assembly: AssemblyDescriptionAttribute("Syntactic (Semantic) Versioning for .NET libraries heavily inspired in elm-package (bump and diff)")>]
 [<assembly: AssemblyVersionAttribute("0.0.1")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.1")>]
@@ -12,7 +12,7 @@ do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "SynVer"
-    let [<Literal>] AssemblyProduct = "SynVer"
+    let [<Literal>] AssemblyProduct = "SyntacticVersioning"
     let [<Literal>] AssemblyDescription = "Syntactic (Semantic) Versioning for .NET libraries heavily inspired in elm-package (bump and diff)"
     let [<Literal>] AssemblyVersion = "0.0.1"
     let [<Literal>] AssemblyFileVersion = "0.0.1"
