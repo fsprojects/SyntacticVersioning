697c63c449d29ffc0249ef85f1631bcbc67e020e
Oskar Gewalli <oskar@gewalli.se>
2017-03-15 20:05:32 +0100

    SynVer.FAKE

---
+++ Fake/SynVer.FAKE/SynVerHelpers.fs
@@ -0,0 +1,13 @@
+﻿namespace SynVer.FAKE
+open Fake
+open Fake.NuGetVersion
+open Fake.SemVerHelper
+open SynVer.Core
+[<AutoOpen>]
+module SynVerHelpers=
+  let bump (version:SemVerInfo) released modified =
+      let (_,magnitude) = SurfaceArea.bump "0.0.0" released modified
+      match magnitude with
+      | Major -> IncMajor version
+      | Minor -> IncMinor version
+      | Patch -> IncPatch version
