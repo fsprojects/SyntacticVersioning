8be0622fafd1f8090ad39a7744172b2e80e8951a
Oskar Gewalli <oskar@gewalli.se>
2017-03-18 16:37:36 +0100

    Release 0.0.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -3,3 +3,6 @@
 #### 0.0.2 - March 12 2017
 * Changed name of the assembly
 * Cleaner interface
+#### 0.0.3 - March 18 2017
+* Renamed SynVer.Core to SynVer.Lib
+* Should have all DLL dependencies included in SynVer Tool
--- src/SynVer.Lib/AssemblyInfo.fs
+++ src/SynVer.Lib/AssemblyInfo.fs
@@ -2,18 +2,18 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("SynVer.Core")>]
+[<assembly: AssemblyTitleAttribute("SynVer.Lib")>]
 [<assembly: AssemblyProductAttribute("SynVer")>]
 [<assembly: AssemblyDescriptionAttribute("Syntactic (Semantic) Versioning for .NET libraries heavily inspired in elm-package (bump and diff)")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyTitle = "SynVer.Core"
+    let [<Literal>] AssemblyTitle = "SynVer.Lib"
     let [<Literal>] AssemblyProduct = "SynVer"
     let [<Literal>] AssemblyDescription = "Syntactic (Semantic) Versioning for .NET libraries heavily inspired in elm-package (bump and diff)"
-    let [<Literal>] AssemblyVersion = "0.0.2"
-    let [<Literal>] AssemblyFileVersion = "0.0.2"
+    let [<Literal>] AssemblyVersion = "0.0.3"
+    let [<Literal>] AssemblyFileVersion = "0.0.3"
     let [<Literal>] AssemblyConfiguration = "Release"
